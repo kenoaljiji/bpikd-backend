dbb9aa18db7d1fbd66d84e158c24289c6aac6df2
kenoaljiji <keno.a89@gmail.com>
2024-03-22 10:46:06 +0100

    add data to server

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://bpikd-backend.onrender.com"
+    "https://bpikd-backend.vercel.app"
   );
   res.header(
     "Access-Control-Allow-Headers",
