d5c6509e811146b7c54fdecf68257a3ee885c9cc
kenoaljiji <keno.a89@gmail.com>
2024-03-22 11:20:20 +0100

    add a onrender.com cors

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://bpikd-backend.vercel.app"
+    "https://bpikd-backend.onrender.com"
   );
   res.header(
     "Access-Control-Allow-Headers",
